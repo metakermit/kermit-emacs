bf4a1a8aced73c7f07a9b7d3d49e4297a879dfa8
Dražen Lučanin <kermit666@gmail.com>
2014-11-04 17:29:23 +0100

    auto-indent code

--- .emacs.d/init/config.el
+++ .emacs.d/init/config.el
@@ -4,4 +4,5 @@
 (load-relative "./uncategorised")
 (load-relative "./multi-term")
 (load-relative "display-info")
+(load-relative "general-editing")
 (load-relative "./os/os")
---
+++ .emacs.d/init/general-editing.el
@@ -0,0 +1,5 @@
+;; General editing behaviour
+;; (auto-indentation etc.)
+
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
