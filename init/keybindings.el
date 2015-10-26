4fc853522bf17db93480f0c96251ebf277db95b8
Dražen Lučanin <kermit666@gmail.com>
2015-10-26 22:01:22 +0100

    C-c d, C-c l to get dark/light solarized

--- init/keybindings.el
+++ init/keybindings.el
@@ -2,4 +2,20 @@
 ;; - convention based on http://stackoverflow.com/a/5682758/544059
 
 ;; a more useful grep (only for versioned files)
-(global-set-key (kbd "C-c g") (negative-argument projectile-grep))
+(global-set-key (kbd "C-c g") 'projectile-grep)
+
+(global-set-key (kbd "C-c e") 'eval-buffer)
+
+(defun set-solarized-light ()
+  (interactive)
+  (customize-set-variable 'frame-background-mode 'light)
+  (load-theme 'solarized t))
+
+(defun set-solarized-dark ()
+  (interactive)
+  (customize-set-variable 'frame-background-mode 'dark)
+  (load-theme 'solarized t))
+
+;; color theme
+(global-set-key (kbd "C-c l") 'set-solarized-light)
+(global-set-key (kbd "C-c d") 'set-solarized-dark)
