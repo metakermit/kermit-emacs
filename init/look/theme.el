00af4bbc4d25b8344b21b726c84994061201919f
Dražen Lučanin <kermit666@gmail.com>
2015-11-01 22:52:13 +0100

    modify to make nicer in terminal & GUI

--- init/look/theme.el
+++ init/look/theme.el
@@ -4,16 +4,20 @@
 ;; Globally map C-c t to a light/dark theme switcher
 ;; Also pull-in graphene for better fonts
 
-(custom-set-variables '(solarized-termcolors 256))
+;; metakermit: modified to be more similar to official README instructions
+;; for changing the theme
+
+;; (custom-set-variables '(solarized-termcolors 256))
 
 (setq solarized-default-background-mode 'dark)
 
 (load-theme 'solarized t)
 
 (defun set-background-mode (frame mode)
-  (set-frame-parameter frame 'background-mode mode)
-  (when (not (display-graphic-p frame))
-    (set-terminal-parameter (frame-terminal frame) 'background-mode mode))
+  ;; (set-frame-parameter frame 'background-mode mode)
+  ;; (when (not (display-graphic-p frame))
+  ;;   (set-terminal-parameter (frame-terminal frame) 'background-mode mode))
+  (customize-set-variable 'frame-background-mode mode)
   (enable-theme 'solarized))
 
 (defun switch-theme ()
@@ -26,5 +30,12 @@
           (lambda (frame) (set-background-mode frame solarized-default-background-mode)))
 
 (set-background-mode nil solarized-default-background-mode)
+
+
+(defun switch-theme ()
+  (interactive)
+  (let ((mode  (if (eq (frame-parameter nil 'background-mode) 'dark)
+                   'light 'dark)))
+    (set-background-mode nil mode)))
 
 (global-set-key (kbd "C-c t") 'switch-theme)
